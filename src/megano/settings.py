c881e25cdbf7b15acb7007fa6a74378971eb950a
Максим Олесов <windigotp@gmail.com>
2023-11-28 19:24:32 +0300

    Footer and header templates

--- src/megano/settings.py
+++ src/megano/settings.py
@@ -69,7 +69,13 @@ TEMPLATES = [
             "constants": {
             },
             'globals': {
-            }
+            },
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
         },
     },
     {
@@ -136,6 +142,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
