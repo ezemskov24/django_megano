239619c3eed90b122b414fdb9c57ddf651f00bf1
Maxim Olesov <windigotp@gmail.com>
2023-11-30 17:29:05 +0800

    Banner caching implemented

--- src/megano/settings.py
+++ src/megano/settings.py
@@ -152,3 +152,11 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.filebased.FileBasedCache",
+        "LOCATION": "/var/tmp/django_cache",
+        "TIMEOUT": 600,
+    }
+}
--- src/products/banner.py
+++ src/products/banner.py
@@ -1,17 +1,33 @@
 import random
 
+from django.core.cache import cache
 from django.db.models import Min
-from django.db.models.functions import Round
 
 from .models import Category, Product
 
 
+FIXED_KEY = 'banners_fixed'
+SLIDER_KEY = 'banners_slider'
+
+
 class Banner:
     def __init__(self, fixed_amount=3, slider_amount=3):
-        self.fixed = random.choices(Category.objects.all(), k=fixed_amount)
-        for category in self.fixed:
-            category.sample = category.products.annotate(
-                min=Min('sellerproduct__price'),
-            ).order_by('min').prefetch_related('images').get()
+        self.fixed = cache.get(FIXED_KEY)
+        if not self.fixed:
+            self.fixed = random.choices(
+                Category.objects.all(),
+                k=fixed_amount,
+            )
+            for category in self.fixed:
+                category.sample = category.products.annotate(
+                    min=Min('sellerproduct__price'),
+                ).order_by('min').prefetch_related('images').get()
+            cache.set(FIXED_KEY, self.fixed)
 
-        self.slider = random.choices(Product.objects.all(), k=fixed_amount)
+        self.slider = cache.get(SLIDER_KEY)
+        if not self.slider:
+            self.slider = random.choices(
+                Product.objects.all(),
+                k=slider_amount,
+            )
+            cache.set(SLIDER_KEY, self.slider)
