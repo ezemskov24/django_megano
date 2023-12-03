31929115708a6f851aecb1f349ecf3cd0a65b573
e_zemskov <zemskoff24@gmail.com>
2023-12-03 21:43:31 +0100

    Model for admin settings

--- src/adminsettings/admin.py
+++ src/adminsettings/admin.py
@@ -1,3 +1,25 @@
-from django.contrib import admin
+from django.contrib import admin, messages
+from django.core.cache import cache
 
-# Register your models here.
+from .models import SiteSettings
+
+
+@admin.register(SiteSettings)
+class SiteSettingsAdmin(admin.ModelAdmin):
+    list_display = [
+        'reviews_count',
+        'products_per_page',
+        'delivery_cost',
+        'express_delivery_cost',
+        'discount_percentage',
+        'products_images',
+        'amount_products_from_the_seller'
+    ]
+
+    actions = ['reset_cache_action']
+
+    def reset_cache_action(self, request, queryset):
+        cache.clear()
+        self.message_user(request, ("Кеш успешно сброшен."), messages.SUCCESS)
+
+    reset_cache_action.short_description = ("Сбросить кеш")
--- src/adminsettings/models.py
+++ src/adminsettings/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class SiteSettings(models.Model):
+    reviews_count = models.PositiveIntegerField(default=10, verbose_name='Количество отзывов')
+    products_per_page = models.PositiveIntegerField(default=20, verbose_name='Товаров на странице')
+    delivery_cost = models.PositiveIntegerField(default=100, verbose_name='Стоимость доставки')
+    express_delivery_cost = models.PositiveIntegerField(default=600, verbose_name='Стоимость экспресс доставки')
+    discount_percentage = models.PositiveIntegerField(default=0, verbose_name='Скидка на все товары')
+    products_images = models.PositiveIntegerField(default=3, verbose_name='Количество изображений в карточке товара')
+    amount_products_from_the_seller = models.PositiveIntegerField(default=20,
+                                                                  verbose_name="количество товаров от одного продавца")
