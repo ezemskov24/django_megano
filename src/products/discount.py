42034f3ee3d7377861124556634c430d7efd0929
Vadim Moiseev <moiseev1ipad@gmail.com>
2023-12-04 12:42:21 +0300

    Creating stubs for cart, compare products and receiving discount

---
+++ src/products/discount.py
@@ -0,0 +1,13 @@
+def get_all_discount(products):
+    '''метод возвращает все скидки на список товаров(или на 1 товар)'''
+    pass
+
+
+def get_priority_discount(products):
+    '''метод возвращает приоритетную скидку на список товаров(или на 1 товар)'''
+    pass
+
+
+def get_product_discount_price(price):
+    '''метод возвращает цену товара с учетом скидки'''
+    pass
