97913e1d2bcea9acf63cef4ff4f8da0ce30a31f9
baranov <barilya@yandex.ru>
2023-12-04 21:29:48 +0500

    file deleted

--- src/account/views.py
+++ src/account/views.py
@@ -8,6 +8,10 @@ from django.views.generic.edit import CreateView
 class UserLoginView(LoginView):
     template_name = 'registration/login.jinja2'
     redirect_authenticated_user = True
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        print(context['form'].as_p())
+        return context
 
 # Logout
 class UserLogoutView(LogoutView):
