43756aa28739a3c0adc3de8a47b5d4c9058a6982
Maxim Olesov <windigotp@gmail.com>
2023-12-13 18:28:54 +0800

    Catalog sorting added

--- src/products/views.py
+++ src/products/views.py
@@ -1,6 +1,8 @@
+from enum import Enum
 from typing import Any, Dict
 
 from django.core.paginator import EmptyPage, Paginator, PageNotAnInteger
+from django.db.models import Min, Max, Count
 from django.http import HttpRequest, HttpResponse
 from django.views.generic import ListView, TemplateView
 
@@ -25,8 +27,30 @@ class CatalogView(ListView):
     model = Product
     context_object_name = 'products'
 
+    class SortEnum(Enum):
+        POP_ASC = '-pop'
+        POP_DEC = 'pop'
+        PRI_ASC = '-pri'
+        PRI_DEC = 'pri'
+        REV_ASC = '-rev'
+        REV_DEC = 'rev'
+        CRE_ASC = '-cre'
+        CRE_DEC = 'cre'
+        NONE = 'none'
+
     def get_queryset(self):
+        sort = self.request.GET.get('sort')
+        if sort:
+            if (sort == self.SortEnum.NONE.value and
+                    self.request.session.get('sort')):
+                del self.request.session['sort']
+            else:
+                self.request.session['sort'] = sort
+        sort = self.request.session.get('sort')
+
         products_list = Product.active.all()
+        if sort:
+            products_list = self.get_sorted_queryset(products_list, sort)
         paginator = Paginator(products_list, 8)
         page_number = self.request.GET.get('p', 1)
         try:
@@ -37,6 +61,45 @@ class CatalogView(ListView):
             products = paginator.page(paginator.num_pages)
 
         return products
+
+    def get_sorted_queryset(self, queryset, sort):
+        if sort == self.SortEnum.CRE_ASC.value:
+            return queryset.order_by('created_at').all()
+        if sort == self.SortEnum.CRE_DEC.value:
+            return queryset.order_by('-created_at').all()
+        if sort == self.SortEnum.POP_ASC.value:
+            return queryset.order_by('-count_sells').all()
+        if sort == self.SortEnum.POP_DEC.value:
+            return queryset.order_by('count_sells').all()
+        if sort == self.SortEnum.PRI_ASC.value:
+            return queryset.annotate(
+                price=Min('sellerproduct__price')
+            ).order_by('-price').all()
+        if sort == self.SortEnum.PRI_DEC.value:
+            return queryset.annotate(
+                price=Min('sellerproduct__price')
+            ).order_by('price').all()
+        # Обёрнуто в try-except, чтобы избегать исключений, пока не будут готовы обзоры
+        try:
+            if sort == self.SortEnum.REV_ASC.value:
+                return queryset.annotate(
+                    rev_count=Count('reviews')
+                ).order_by('-rev_count').all()
+            if sort == self.SortEnum.REV_DEC.value:
+                return queryset.annotate(
+                    rev_count=Count('reviews')
+                ).order_by('rev_count').all()
+        except:
+            pass
+        return queryset
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['sort'] = self.SortEnum
+        curr_sort = self.request.session.get('sort')
+        if curr_sort:
+            context['curr_sort'] = curr_sort
+        return context
 
 
 def ProductCreateView():
