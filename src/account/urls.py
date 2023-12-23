c8c000920a584596899ce4f9bdbc9a0712fa439b
baranov <barilya@yandex.ru>
2023-12-23 17:43:36 +0500

    profile validation

--- src/account/urls.py
+++ src/account/urls.py
@@ -1,8 +1,16 @@
-from django.contrib.auth.views import LoginView
+# from django.contrib.auth.views import LoginView
 from django.urls import path
-from .views import UserLogoutView, UserLoginView, RegisterView, UserAccountView, ProfileUpdateView
-from .views import UserLogoutView, UserProfileView, UserLoginView, SellerDetailView
+from .views import (
+    UserLogoutView,
+    UserLoginView,
+    RegisterView,
+    UserAccountView,
+    ProfileUpdateView,
+    UserEmailView,
+    SellerDetailView
+)
 
+# from .views import UserProfileView, SellerDetailView
 # from django.contrib.auth.views import PasswordResetDoneView, PasswordResetConfirmView, PasswordResetCompleteView
 
 app_name = 'account'
@@ -11,6 +19,7 @@ urlpatterns = [
     path('login/', UserLoginView, name='login'),
     path('registr/', RegisterView.as_view(), name='registr'),
     path('logout/', UserLogoutView.as_view(), name='logout'),
+    path('e-mail/', UserEmailView.as_view(), name='e-mail'),
     path("seller/<int:pk>/", SellerDetailView.as_view(), name="seller_details"),
     path('profile/', ProfileUpdateView.as_view(), name='profile'),
     path('account/', UserAccountView.as_view(), name='account'),
