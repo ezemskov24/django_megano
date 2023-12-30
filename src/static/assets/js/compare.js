a8391e0ac94dee77ca003db3348e336b1a7dab35
Vadim Moiseev <moiseev1ipad@gmail.com>
2023-12-30 16:14:26 +0300

    stage 4: fix 2

--- src/static/assets/js/compare.js
+++ src/static/assets/js/compare.js
@@ -1,1 +1,15 @@
-//файл для будущего динамического отображения страницы сравнения
+async function delete_all() {
+    await fetch('delete_all/')
+    document.getElementById('all_products').style.display = 'none'
+    compare_length()
+}
+
+async function delete_product(slug) {
+    document.getElementById(slug).style.display = 'none'
+    await fetch('delete/' + slug + '/')
+    compare_length()
+}
+
+function change_properties() {
+    console.log(1)
+}
