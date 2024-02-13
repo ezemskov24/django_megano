63b89a1c39442b9e4c9f48e1bbb696e3075b7680
aleksejpavlenok <alexey_pavlenok@mail.ru>
2024-02-13 13:47:05 +0300

    stage 4. Fix. Card delete after ordering added

--- src/cart/views.py
+++ src/cart/views.py
@@ -84,8 +84,7 @@ class CreateOrderView(LoginRequiredMixin, View):
         form = CreateOrderForm(request.POST)
         if form.is_valid():
             form.save()
-            # удалить товары из корзины
-            # Cart.objects.filter(profile=request.user.id).delete()
+            Cart.objects.filter(profile=request.user.id).delete()
 
             paid_url = get_paid(Order.objects.filter(profile=request.user.pk).last())
             return redirect(paid_url)
