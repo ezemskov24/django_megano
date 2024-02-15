689414ee9d97b2a3dbbcde4c98f5aa0b574b9caf
aleksejpavlenok <alexey_pavlenok@mail.ru>
2024-02-15 20:58:18 +0300

    stage 4. Fix. Cart.admin changed, str added

--- src/cart/admin.py
+++ src/cart/admin.py
@@ -14,19 +14,12 @@ class PropertyValueAdmin(admin.ModelAdmin):
     ]
 
 
-# class OrderInLine(admin.TabularInline):
-#     model = Order
-
-
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = 'profile', 'fio', 'phone', 'email', \
                    'city', 'delivery_address', 'delivery_type', \
                    'payment_type', 'comment', 'archived', 'created_at'
     ordering = 'created_at',
-    # inlines = [
-    #         OrderInLine,
-    #     ]
 
-    # def get_queryset(self, request):
-    #     return Order.objects.select_related("cart")
+    def __str__(self):
+        return f"Заказ № {self.id}"
